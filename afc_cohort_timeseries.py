2fa68e1dd822c532985a74b3228a7705396745e8
Mark Wells <wellowdata@gmail.com>
2020-11-27 12:11:14 +0000

    pretty output and %orig balance
    added output for excel the same as AFC's
    added code for % of original balance

--- afc_cohort_timeseries.py
+++ afc_cohort_timeseries.py
@@ -138,19 +138,28 @@ cohort_contract_sum_df.sort_index(inplace=True)
 
 full_timeseries_df = pd.merge(
     cohort_contract_sum_df['TotalContractValue'].to_frame(),
-    df4,
+    cumsum_timeseries,
     left_index=True,
     right_index=True)
 
-amort_timeseries_df = -df4.add(cohort_contract_sum_df['TotalContractValue'], axis=0)
-amort_timeseries_df.to_csv('temp7.csv')
-
 ## dafuq is happening here??? how can adding two negatives give the right answer?
-full_df = -df4.add(-cohort_contract_sum_df['TotalContractValue'], axis='index')
+amort_timeseries_df = -cumsum_timeseries.add(-cohort_contract_sum_df['TotalContractValue'], axis=0)
+amort_timeseries_df.to_csv('temp2.csv')
 
-full_df.to_csv('temp8.csv')
-df_to_plot = full_df['2017-06-01':]
+#full_df = -df4.add(-cohort_contract_sum_df['TotalContractValue'], axis='index')
+
+perc_orig_balance_timeseries_df = -cumsum_timeseries.divide(-cohort_contract_sum_df['TotalContractValue'], axis=0)
+
+
+df_to_plot = amort_timeseries_df['2017-06-01':'2020-06-01']
+contract_vals_df = cohort_contract_sum_df['2017-06-01':'2020-06-01']
 df_to_plot.index = df_to_plot.index.month_name() + df_to_plot.index.year.astype('str')
 ax = df_to_plot.T.loc[0:].plot(xticks=range(35), figsize=(20,8))
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000000) + 'm'))
 plt.savefig('amortization line chart.png')
+
+contract_vals_df.index = contract_vals_df.index.month_name() + \
+    contract_vals_df.index.year.astype('str')
+pd.concat(
+    [contract_vals_df['TotalContractValue'], df_to_plot.loc[:,0:42], ], axis=1,
+          ).to_csv('Graph_Amort_MW.csv')
