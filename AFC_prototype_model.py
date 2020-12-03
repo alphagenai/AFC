8b5bd5cce6bf46739649239933b3e655da057f40
Mark Wells <wellowdata@gmail.com>
2020-12-03 17:45:10 +0000

    created regression model

--- AFC_prototype_model.py
+++ AFC_prototype_model.py
@@ -6,30 +6,54 @@ Created on Wed Dec  2 14:48:47 2020
 """
 
 import pandas as pd
+import datetime as dt
 
-from individual_analysis1 import small_df
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+
+from individual_analysis1 import small_df, cumulative_percent_sdf
+
+
 
 ## assemble features
 
-contract_sql = """
-    SELECT c.*,
-        Price + AdditionalFee as TotalContractValue,     
-    FROM `afcproj.files_dupe.Contracts_20201117` c
-    join `afcproj.files_dupe.jan_19_cohort` j
-        on c.ContractId = j.ContractId
-        """
-cdf = pd.read_gbq(contract_sql,index_col='ContractId') #.astype('float64')
+if 0:
+    contract_sql = """
+        SELECT c.*,
+            Price + AdditionalFee as TotalContractValue,     
+        FROM `afcproj.files_dupe.Contracts_20201117` c
+        join `afcproj.files_dupe.jan_19_cohort` j
+            on c.ContractId = j.ContractId
+            """
+    cdf = pd.read_gbq(contract_sql,index_col='ContractId') #.astype('float64')
+    
+    
+    all_features = pd.merge(small_df,
+             cdf,
+             how='inner',
+             left_index=True,
+             right_index=True).sort_index()
 
 
-all_features = pd.merge(small_df,
-         cdf,
-         how='inner',
-         left_index=True,
-         right_index=True).sort_index()
+#### Super Simple Regression Model
+#currently only works for one contract at a time
+df = cumulative_percent_sdf[cumulative_percent_sdf.columns[0:3]] 
+diff_df = df.diff()
+monthly = df.groupby(pd.Grouper(freq='M')).sum().cumsum().stack().reset_index()
 
 
-#1. lets look at if theres any autocorrelation. what lag?
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(monthly['ContractId'].array.reshape(-1,1))
 
-for contract_series in data[0:5]:
-    plot_acf(series, lags=20)
-    plt.show()
+ols = LinearRegression()
+X = monthly.index.map(dt.datetime.toordinal).to_numpy().reshape(-1,1)
+y = monthly.to_numpy()
+ols.fit(X, y)
+y_pred = ols.predict(X)
+
+# Plot
+plt.scatter(X, y,  color='black')
+plt.plot(X, y_pred, color='blue', linewidth=3)
+
+
+
--- individual_analysis1.py
+++ individual_analysis1.py
@@ -19,169 +19,183 @@ import matplotlib.ticker as ticker
 
 #client = bigquery.Client()
 
+def create_small_df():    
+    SQL = """ 
+        Select p.TransactionTS,
+            p.AmountPaid,
+            p.ContractId
+        FROM afcproj.files_dupe.Payments_2020_11_17 p
+        inner join afcproj.files_dupe.jan_19_cohort j
+            on p.ContractId = j.ContractId   
+        inner join afcproj.files_dupe.Contracts_20201117 c
+            on c.ContractId = j.ContractId
+        WHERE p.paymentStatusTypeEntity != 'REFUSED'
+            and
+            p.PaymentResultTypeEntity != 'PAYMENT_FREE_CREDIT'
+            and (c.Product = 'X850'
+            or c.Product = 'X850 Plus')
+        UNION ALL
+        Select a.CreatedAt,
+            a.Amount,
+            a.ContractId
+        FROM afcproj.files_dupe.Adjustments_2020_11_17 a
+        inner join afcproj.files_dupe.jan_19_cohort j
+            on a.ContractId = j.ContractId
+        inner join afcproj.files_dupe.Contracts_20201117 c
+            on c.ContractId = j.ContractId
+    
+        WHERE a.BalanceChangeType = 'MANUAL'
+            and (c.Product = 'X850'
+            or c.Product = 'X850 Plus')
+    
+            """
+    
+    #for contractID in cohort:
+    df = pd.read_gbq(SQL,) #chunksize=10000) #chunksize doesnt work
+    
+    df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S')
+    
+    df = df.set_index(['ContractId','TransactionTS'])
+                  
+    df = df.astype('float64')
+    
+    
+    MEAN_PAYMENT = df.mean()
+    
+    
+    hundred_random_IDs = random.sample(df.index.get_level_values(0).unique().values.tolist(), k=100)
+    
+    small_df = df.loc[hundred_random_IDs]   # see which IDs --> small_df.index.get_level_values(0).unique()
+    return small_df
 
-SQL = """ 
-    Select p.TransactionTS,
-        p.AmountPaid,
-        p.ContractId
-    FROM afcproj.files_dupe.Payments_2020_11_17 p
-    inner join afcproj.files_dupe.jan_19_cohort j
-        on p.ContractId = j.ContractId   
-    inner join afcproj.files_dupe.Contracts_20201117 c
-        on c.ContractId = j.ContractId
-    WHERE p.paymentStatusTypeEntity != 'REFUSED'
-        and
-        p.PaymentResultTypeEntity != 'PAYMENT_FREE_CREDIT'
-        and c.Product = 'X850'
-    UNION ALL
-    Select a.CreatedAt,
-        a.Amount,
-        a.ContractId
-    FROM afcproj.files_dupe.Adjustments_2020_11_17 a
-    inner join afcproj.files_dupe.jan_19_cohort j
-        on a.ContractId = j.ContractId
-    inner join afcproj.files_dupe.Contracts_20201117 c
-        on c.ContractId = j.ContractId
-
-    WHERE a.BalanceChangeType = 'MANUAL'
-        and c.Product = 'X850'
-
+def create_cumulative_percent_sdf(daily_sdf):
+    
+    
+    ### Get Contract info
+    
+    SQL = """
+        SELECT c.ContractId,
+            Price + AdditionalFee as TotalContractValue,     
+            --c.RegistrationDate 
+        FROM `afcproj.files_dupe.Contracts_20201117` c
+        join `afcproj.files_dupe.jan_19_cohort` j
+            on c.ContractId = j.ContractId
         """
+    cdf = pd.read_gbq(SQL,index_col='ContractId').astype('float64')
+    
+    
+    contract_ts = pd.merge(
+        daily_sdf.T,
+        cdf,
+        how='inner',
+        left_index=True,
+        right_index=True)
+    
+    contract_values = contract_ts['TotalContractValue']
+    
+    percent_df = contract_ts.divide(contract_values, axis=0).drop(columns=['TotalContractValue'])
+    
+    cumulative_percent_sdf = percent_df.T.cumsum(axis=0)
+    cumulative_percent_sdf.index = pd.to_datetime(cumulative_percent_sdf.index,format='%Y/%m/%d %H:%M:%S')
+    
+    return cumulative_percent_sdf
 
-#for contractID in cohort:
-df = pd.read_gbq(SQL,) #chunksize=10000) #chunksize doesnt work
-
-df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S')
-
-df = df.set_index(['ContractId','TransactionTS'])
-              
-df = df.astype('float64')
-
-
-MEAN_PAYMENT = df.mean()
-
-
-hundred_random_IDs = random.sample(df.index.get_level_values(0).unique().values.tolist(), k=100)
-
-small_df = df.loc[hundred_random_IDs]   # see which IDs --> small_df.index.get_level_values(0).unique()
-
+small_df = create_small_df()
 sdf = small_df['AmountPaid'].unstack(0).fillna(0).sort_index()
 
-
-
-
-### Get Contract info
-
-SQL = """
-    SELECT c.ContractId,
-        Price + AdditionalFee as TotalContractValue,     
-        --c.RegistrationDate 
-    FROM `afcproj.files_dupe.Contracts_20201117` c
-    join `afcproj.files_dupe.jan_19_cohort` j
-        on c.ContractId = j.ContractId
-    """
-cdf = pd.read_gbq(SQL,index_col='ContractId').astype('float64')
-
-
-df = pd.merge(
-    sdf.T,
-    cdf,
-    how='inner',
-    left_index=True,
-    right_index=True)
-
-contract_values = df['TotalContractValue']
-
-percent_df = df.divide(contract_values, axis=0).drop(columns=['TotalContractValue'])
-
-cumulative_percent_sdf = percent_df.T.cumsum(axis=0)
-cumulative_percent_sdf.index = pd.to_datetime(cumulative_percent_sdf.index,format='%Y/%m/%d %H:%M:%S')
-
-
-
 daily_sdf = sdf.groupby(sdf.index.date).sum() #all payments in one day are grouped together
+    
+    
 daily_cum_sdf = daily_sdf.cumsum(axis=0) 
 monthly_sdf = sdf.groupby(pd.Grouper(freq='M')).sum()
 monthly_cum_sdf = monthly_sdf.cumsum(axis=0)
 
-timeseries_length = (daily_sdf.index.max() - daily_sdf.index.min()).days
-
-AVERAGE_PAYMENT_FREQUENCY = timeseries_length / daily_sdf.astype(bool).sum(axis=0) 
-MEAN_PAYMENT_PER_DAY = daily_sdf.sum()/ timeseries_length
-
-###### Plotting
-## Daily Payments
-ax = daily_cum_sdf.plot(figsize=(20,8), legend=False, title="Cumulative Payments for 100 Random Contracts in Jan 2019 Cohort")
-ax.set_xlabel("Transaction Date")
-ax.set_ylabel("Total Repayment Amount")
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000)+'k'))
-plt.savefig('Cumulative Payments for 100 Random X850 Contracts in Jan 2019 Cohort')
 
 
-## Smoothed Payments
-daily_cum_sdf.rolling(window=30).mean().plot(figsize=(20,8), legend=False, title="Smoothed Cumulative Payments for 100 Random Contracts in Jan 2019 Cohort")
-ax.set_xlabel("Transaction Date")
-ax.set_ylabel("Total Repayment Amount")
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000)+'k'))
-plt.savefig('Smoothed Cumulative Payments for 100 Random  X850 Contracts in Jan 2019 Cohort')
+if __name__ == "__main__":
+    timeseries_length = (daily_sdf.index.max() - daily_sdf.index.min()).days
 
 
-
-## Percentage Payments
-cumulative_percent_sdf.plot(figsize=(20,8), legend=False, title="Cumulative % Payments for 100 Random Contracts in Jan 2019 Cohort")
-ax.set_xlabel("Transaction Date")
-ax.set_ylabel("Total Repayment Amount % of Contract Value")
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0%}'.format(x)))
-plt.savefig('Cumulative percentage Payments for 100 Random Contracts in Jan 2019 Cohort')
-
-
-
-## Monthly Percentage Payments
-
-monthly_cum_perc_df = monthly_cum_sdf.divide(contract_values, axis=1)
-
-monthly_cum_perc_df.plot(figsize=(20,8), legend=False, title="Monthly Cumulative % Payments for 100 Random Contracts in Jan 2019 Cohort")
-ax.set_xlabel("Transaction Date")
-ax.set_ylabel("Total Repayment Amount % of Contract Value")
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0%}'.format(x)))
-plt.savefig('Monthly Cumulative percentage Payments for 100 Random Contracts in Jan 2019 Cohort')
-
-
-## HISTAGRAM OF daily % rePAYMENTS
-
-payment_series_array =  percent_df.stack().to_numpy()
-daily_nonzero_percent_payments = payment_series_array[payment_series_array.nonzero()]
-
-f, ax = plt.subplots(figsize=(7, 5))
-plt.yscale('log')
-ax = sns.histplot(daily_nonzero_percent_payments, bins=100, )
-ax.set_title('Daily Payment Histogram')
-ax.set_xlabel('Payment %')
-ax.set_ylabel('Count')
-ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:.0%}'.format(x)))
-plt.savefig('Daily Payment Histogram.png')
-
-
-
-## monthly HISTAGRAM OF daily % rePAYMENTS
-
-
-df = pd.merge(
-    monthly_sdf.T,
-    cdf,
-    how='inner',
-    left_index=True,
-    right_index=True)
-
-percent_month_df = df.divide(df['TotalContractValue'], axis=0).drop(columns=['TotalContractValue'])
-month_payment_series_array =  percent_month_df.stack().to_numpy()
-monthly_nonzero_percent_payments = month_payment_series_array[month_payment_series_array.nonzero()]
-
-f, ax = plt.subplots(figsize=(7, 5))
-#plt.yscale('log')
-ax = sns.histplot(monthly_nonzero_percent_payments, bins=100, )
-ax.set_title('Monthly Payment Histogram')
-ax.set_xlabel('Payment %')
-ax.set_ylabel('Count')
-ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:.0%}'.format(x)))
-plt.savefig('Monthly Payment Histogram.png')
+    AVERAGE_PAYMENT_FREQUENCY = timeseries_length / daily_sdf.astype(bool).sum(axis=0) 
+    MEAN_PAYMENT_PER_DAY = daily_sdf.sum()/ timeseries_length
+    
+    ###### Plotting
+    ## Daily Payments
+    title = "Cumulative Payments for 100 Random X850 Contracts in Jan 2019 Cohort"
+    ax = daily_cum_sdf.plot(figsize=(20,8), legend=False, title=title)
+    ax.set_xlabel("Transaction Date")
+    ax.set_ylabel("Total Repayment Amount")
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000)+'k'))
+    plt.savefig(title)
+    
+    
+    ## Smoothed Payments
+    title = "Smoothed Cumulative Payments for 100 Random X850 Contracts in Jan 2019 Cohort"
+    ax = daily_cum_sdf.rolling(window=30).mean().plot(figsize=(20,8), legend=False, title=title)
+    ax.set_xlabel("Transaction Date")
+    ax.set_ylabel("Total Repayment Amount")
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000)+'k'))
+    plt.savefig(title)
+    
+    
+    
+    ## Percentage Payments
+    cumulative_percent_sdf = create_cumulative_percent_sdf(daily_sdf)
+    title = "Cumulative % Payments for 100 Random X850 Contracts in Jan 2019 Cohort"
+    ax = cumulative_percent_sdf.plot(figsize=(20,8), legend=False, title=title)
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0%}'.format(x)))
+    ax.set_xlabel("Transaction Date")
+    ax.set_ylabel("files\\Total Repayment Amount % of Contract Value")
+    plt.savefig(title)
+    
+    
+    
+    ## Monthly Percentage Payments
+    
+    monthly_cum_perc_df = monthly_cum_sdf.divide(contract_values, axis=1)
+    
+    monthly_cum_perc_df.plot(figsize=(20,8), legend=False, title="Monthly Cumulative % Payments for 100 Random Contracts in Jan 2019 Cohort")
+    ax.set_xlabel("Transaction Date")
+    ax.set_ylabel("Total Repayment Amount % of Contract Value")
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0%}'.format(x)))
+    plt.savefig('Monthly Cumulative percentage Payments for 100 Random Contracts in Jan 2019 Cohort')
+    
+    
+    ## HISTAGRAM OF daily % rePAYMENTS
+    
+    payment_series_array =  percent_df.stack().to_numpy()
+    daily_nonzero_percent_payments = payment_series_array[payment_series_array.nonzero()]
+    
+    f, ax = plt.subplots(figsize=(7, 5))
+    plt.yscale('log')
+    ax = sns.histplot(daily_nonzero_percent_payments, bins=100, )
+    ax.set_title('Daily Payment Histogram')
+    ax.set_xlabel('Payment %')
+    ax.set_ylabel('Count')
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:.0%}'.format(x)))
+    plt.savefig('Daily Payment Histogram.png')
+    
+    
+    
+    ## monthly HISTAGRAM OF daily % rePAYMENTS
+    
+    
+    df = pd.merge(
+        monthly_sdf.T,
+        cdf,
+        how='inner',
+        left_index=True,
+        right_index=True)
+    
+    percent_month_df = df.divide(df['TotalContractValue'], axis=0).drop(columns=['TotalContractValue'])
+    month_payment_series_array =  percent_month_df.stack().to_numpy()
+    monthly_nonzero_percent_payments = month_payment_series_array[month_payment_series_array.nonzero()]
+    
+    f, ax = plt.subplots(figsize=(7, 5))
+    #plt.yscale('log')
+    ax = sns.histplot(monthly_nonzero_percent_payments, bins=100, )
+    ax.set_title('Monthly Payment Histogram')
+    ax.set_xlabel('Payment %')
+    ax.set_ylabel('Count')
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:.0%}'.format(x)))
+    plt.savefig('Monthly Payment Histogram.png')
