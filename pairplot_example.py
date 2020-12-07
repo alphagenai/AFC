c97c58a3faa5a116c07fafbea321db531dfc22cd
Mark Wells <wellowdata@gmail.com>
2020-12-07 12:06:20 +0000

    pairplots show some interesting features

--- pairplot_example.py
+++ pairplot_example.py
@@ -4,8 +4,34 @@ Created on Mon Dec  7 09:52:09 2020
 
 @author: mark
 """
+
+
 # Import pyplot, figures inline, set style, plot pairplot
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-sns.pairplot(all_features, hue='Age');
+
+
+categorical_columns = [ 'Region',
+                         'Town',
+                         'Occupation',]
+binary_columns = ['MainApplicantGender',]
+
+numerical_columns = ['Age', 'TotalContractValue',]
+
+
+df = pd.merge(all_features,
+         last_payment,
+         left_index=True,
+         right_index=True,
+         )
+
+
+cat_cols = categorical_columns + binary_columns
+
+
+cat_df_to_plot = df[cols+[pd.Timestamp('2020-11-30 00:00:00')]]
+payment_df_to_plot = all_features.drop(columns=cols)
+sns.pairplot(payment_df_to_plot,) #hue='Age');
+
+sns.catplot(x="Occupation", y=pd.Timestamp('2020-11-30 00:00:00'), kind="swarm", data=cat_df_to_plot)
