514c71a63ab4b7e588948a389a8a4d68275882f3
Mark Wells <wellowdata@gmail.com>
2020-12-14 11:00:30 +0000

    an analysis of the types of customer in the dec 17 cohort

--- long_timeseries_analysis.py
+++ long_timeseries_analysis.py
@@ -10,14 +10,17 @@ from individual_analysis1 import create_small_df
 
 small_df = create_small_df(size=1000, cohort='dec_17')
 
+cumulative_percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=True, cohort='dec_17')
+
 at_month_18 = cumulative_percent_sdf.loc['2019-05-31']
 last_chance = cumulative_percent_sdf.loc['2020-11-17']
 
-paid_off_on_time = (at_month_18 >= 1.0)
-paid_off_eventually = (at_month_18 < 1.0) & (last_chance >= 1.0)
-not_paid_ok = (last_chance > 0.8) & (last_chance < 1.0)
+paid_off_on_time = (at_month_18 >= 0.98)
+paid_off_eventually = (at_month_18 < 0.98) & (last_chance >= 0.98)
+not_paid_ok = (last_chance > 0.8) & (last_chance < 0.980)
 bad = (last_chance < 0.8) & (last_chance >= 0.5)
-very_bad = (last_chance < 0.5) 
+very_bad = (last_chance < 0.5) & (last_chance >= 0.2) 
+default = (last_chance < 0.2)  
 
 ## This guy is interesting:
 # cumulative_percent_sdf['1351574'].plot()
@@ -26,12 +29,14 @@ num_paid_off = paid_off.sum()
 num_not_good = NG.sum()
 num_bad = bad.sum()
 
-categories = [paid_off_on_time,
-paid_off_eventually ,
-not_paid_ok ,
-bad ,
-very_bad ,
-]
+categories = {'Paid off on time':paid_off_on_time,
+              'Paid off after 18 months':paid_off_eventually ,
+              'Not fully repaid':not_paid_ok ,
+              'Less than 80% paid':bad ,
+              'Less than 50% paid':very_bad ,
+              'Less than 20% paid':default,
+
+              }
 
 run_sum = 0
 for cat in categories:
@@ -42,3 +47,16 @@ print(run_sum)
 
 
 cumulative_percent_sdf[paid_off_on_time.index[paid_off_on_time]].plot()
+
+
+percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=False, cohort='dec_17')
+
+for cat_name, cat in categories.items():
+    mean_daily_repayment = percent_sdf[cat.index[cat]].mean(axis=1)
+    full_cumulative_repayment = cumulative_percent_sdf[cat.index[cat]]
+
+    fig, ax = plt.subplots()
+    mean_daily_repayment.plot(title=cat_name)
+    fig, ax = plt.subplots()
+    full_cumulative_repayment.plot(title=cat_name)
+    plt.show()
