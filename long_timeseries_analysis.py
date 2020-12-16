680d26749cf3382fb54468645bd6c179975f371c
Mark Wells <wellowdata@gmail.com>
2020-12-16 15:13:09 +0000

    put into functions

--- long_timeseries_analysis.py
+++ long_timeseries_analysis.py
@@ -12,11 +12,6 @@ from matplotlib import pyplot as plt
 
 from individual_analysis1 import create_small_df, create_percent_sdf, convert_to_daily_pivot
 
-small_df = create_small_df(size=1000, cohort='dec_17')
-daily_sdf_pivot = convert_to_daily_pivot(small_df)
-cumulative_percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=True, cohort='dec_17')
-cumulative_amount_sdf = daily_sdf_pivot.cumsum(axis=0)
-cumulative_amount_sdf.index = pd.to_datetime(cumulative_amount_sdf.index,format='%Y/%m/%d %H:%M:%S')
 
 def pick_interesting_timepoints(cumulative_df):
     at_month_6 = cumulative_df.loc['2018-05-30']
@@ -67,7 +62,17 @@ def create_boolean_categories(cumulative_percent_sdf):
     default = (last_chance < 0.2)  
     return paid_off_on_time, paid_off_eventually, gt_80, bad, very_bad, default
 
-categories = {'Paid off on time':paid_off_on_time,
+
+if __name__ == "__main__":
+    
+    small_df = create_small_df(size=1000, cohort='dec_17')
+    daily_sdf_pivot = convert_to_daily_pivot(small_df)
+    cumulative_percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=True, cohort='dec_17')
+    cumulative_amount_sdf = daily_sdf_pivot.cumsum(axis=0)
+    cumulative_amount_sdf.index = pd.to_datetime(cumulative_amount_sdf.index,format='%Y/%m/%d %H:%M:%S')
+    
+
+    categories = {'Paid off on time':paid_off_on_time,
               'Paid off after 18 months':paid_off_eventually ,
               'Almost fully repaid':gt_80 ,
               'Less than 80% paid':bad ,
@@ -76,98 +81,98 @@ categories = {'Paid off on time':paid_off_on_time,
 
               }
 
-run_sum = 0
-for cat_name, cat in categories.items():
-    print(cat_name, cat.sum())
-    run_sum += cat.sum()
-## should add to 1000!
-print(run_sum)
-
-
-cumulative_percent_sdf[paid_off_on_time.index[paid_off_on_time]].plot()
-
-
-percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=False, cohort='dec_17')
-
-
-
-
-fig, ax = plt.subplots()
-sns.histplot(last_chance[gt_80 | bad | very_bad | default], bins=100)
-plt.show()
-
-
-
-## look at individual and average variance
-mean_daily_repayment = {}
-std_daily_repayment = {}
-std_cust_repayment = {}
-full_cumulative_repayment = {}
-
-
-
-for cat_name, cat in categories.items():
-    mean_daily_repayment[cat_name] = percent_sdf[cat.index[cat]].mean(axis=1)
-    std_daily_repayment[cat_name] = percent_sdf[cat.index[cat]].std(axis=1)
-    std_cust_repayment[cat_name] = percent_sdf[cat.index[cat]].std(axis=0)
-    full_cumulative_repayment[cat_name] = cumulative_percent_sdf[cat.index[cat]]
+    run_sum = 0
+    for cat_name, cat in categories.items():
+        print(cat_name, cat.sum())
+        run_sum += cat.sum()
+    ## should add to 1000!
+    print(run_sum)
     
-    # daily average payments across the group
+    
+    cumulative_percent_sdf[paid_off_on_time.index[paid_off_on_time]].plot()
+    
+    
+    percent_sdf = create_percent_sdf(daily_sdf_pivot, cumulative=False, cohort='dec_17')
+    
+    
+    
+    
     fig, ax = plt.subplots()
-    mean_daily_repayment[cat_name].plot(title=cat_name, legend=False)
-    plt.savefig('files\\{}_daily_average_payments'.format(cat_name))
+    sns.histplot(last_chance[gt_80 | bad | very_bad | default], bins=100)
+    plt.show()
     
-    ## what do the cumulative payments look like?
+    
+    
+    ## look at individual and average variance
+    mean_daily_repayment = {}
+    std_daily_repayment = {}
+    std_cust_repayment = {}
+    full_cumulative_repayment = {}
+    
+    
+    
+    for cat_name, cat in categories.items():
+        mean_daily_repayment[cat_name] = percent_sdf[cat.index[cat]].mean(axis=1)
+        std_daily_repayment[cat_name] = percent_sdf[cat.index[cat]].std(axis=1)
+        std_cust_repayment[cat_name] = percent_sdf[cat.index[cat]].std(axis=0)
+        full_cumulative_repayment[cat_name] = cumulative_percent_sdf[cat.index[cat]]
+        
+        # daily average payments across the group
+        fig, ax = plt.subplots()
+        mean_daily_repayment[cat_name].plot(title=cat_name, legend=False)
+        plt.savefig('files\\{}_daily_average_payments'.format(cat_name))
+        
+        ## what do the cumulative payments look like?
+        fig, ax = plt.subplots()
+        full_cumulative_repayment[cat_name].plot(title=cat_name, legend=False)
+        plt.savefig('files\\{}_cumulative_payments'.format(cat_name))
+        
+        ##histograms of daily payments
+        # these are not very useful because most days are zero - might be more 
+        # interesting to look at this on the monthly level
+        fig, ax = plt.subplots()
+        sns.histplot(percent_sdf[cat.index[cat]], bins=100, legend=False)
+        plt.xscale('log')
+        title = 'Histogram of Daily Payments {}'.format(cat_name)
+        plt.title(title)
+        plt.savefig('files\\'+title)
+    plt.show() 
+    
+    
+    for cat_name, cat in categories.items():
+    
+        ## histogram at 6 months
+        fig, ax = plt.subplots()
+        sns.histplot(last_chance[cat.index[cat]], bins=20, legend=False)
+        title = 'Histogram of Cumulative Payments at 3 Years {}'.format(cat_name)
+        plt.title(title)
+        plt.savefig('files\\'+title)
+        
+        ##what are the total number of token bought for each group?
+        
+    plt.show()
+    
+    
+    
+    
+    ## cumulative % payment HISTOGRAM AT 18,24,36 Months
+    
     fig, ax = plt.subplots()
-    full_cumulative_repayment[cat_name].plot(title=cat_name, legend=False)
-    plt.savefig('files\\{}_cumulative_payments'.format(cat_name))
+    sns.histplot(at_month_6)
+    plt.title('6 months')
     
-    ##histograms of daily payments
-    # these are not very useful because most days are zero - might be more 
-    # interesting to look at this on the monthly level
     fig, ax = plt.subplots()
-    sns.histplot(percent_sdf[cat.index[cat]], bins=100, legend=False)
-    plt.xscale('log')
-    title = 'Histogram of Daily Payments {}'.format(cat_name)
-    plt.title(title)
-    plt.savefig('files\\'+title)
-plt.show() 
-
-
-for cat_name, cat in categories.items():
-
-    ## histogram at 6 months
+    sns.histplot(at_month_18)
+    plt.title('18 months')
+    
     fig, ax = plt.subplots()
-    sns.histplot(last_chance[cat.index[cat]], bins=20, legend=False)
-    title = 'Histogram of Cumulative Payments at 3 Years {}'.format(cat_name)
-    plt.title(title)
-    plt.savefig('files\\'+title)
+    sns.histplot(at_month_24)
+    plt.title('24 months')
     
-    ##what are the total number of token bought for each group?
+    fig, ax = plt.subplots()
+    sns.histplot(last_chance)
+    plt.title('3 Years')
     
-plt.show()
-
-
-
-
-## cumulative % payment HISTOGRAM AT 18,24,36 Months
-
-fig, ax = plt.subplots()
-sns.histplot(at_month_6)
-plt.title('6 months')
-
-fig, ax = plt.subplots()
-sns.histplot(at_month_18)
-plt.title('18 months')
-
-fig, ax = plt.subplots()
-sns.histplot(at_month_24)
-plt.title('24 months')
-
-fig, ax = plt.subplots()
-sns.histplot(last_chance)
-plt.title('3 Years')
-
-
-### 
-
+    
+    ### 
+    
