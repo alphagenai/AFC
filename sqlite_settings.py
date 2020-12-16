329729e6004704ba9a38c8871b119c4d86183db3
Mark Wells <wellowdata@gmail.com>
2020-12-16 19:06:30 +0000

    Create sqlite_settings.py

---
+++ sqlite_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 19:05:55 2020
+
+@author: mark
+"""
+
+
+DB_PATH = r'/mnt/disks/disk-1/d_light/sfc.db'
+os.environ["SQLITE_TMPDIR"] = r'/mnt/disks/disk-1/d_light/'
