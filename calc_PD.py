3fb6c61abe5296add484b6024467de3ec3427d4a
Mark Wells <wellowdata@gmail.com>
2020-12-30 17:09:42 +0000

    calc_pd now handle NaN for paid off months

--- calc_PD.py
+++ calc_PD.py
@@ -6,50 +6,87 @@ Created on Tue Dec 29 00:12:12 2020
 """
 
 
+import pandas as pd
+import numpy as np
 
-default = daily_sdf_fullts['PAR30+']
-default_event = (default.diff() != 0) & (daily_sdf_fullts['PAR30+']==True)
-recovery_event = (default.diff() != 0) & (daily_sdf_fullts['PAR30+']==False) & (
-    (default.index.get_level_values(1).month != 12) & (default.index.get_level_values(1).year != 2017)
-    )
-daily_sdf_fullts[recovery_event].to_csv('recoveries.csv')
-daily_sdf_fullts[default_event].to_csv('defauls.csv')
+from individual_analysis1 import create_small_df, create_percent_sdf, convert_to_daily_pivot
+
+
+df = pd.read_pickle('files\\small_df_1000_dec_17.pkl')
+df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S').dt.tz_localize(None)
+
+df = df.groupby(['ContractId', 'TransactionTS']).sum()
+
+daily_sdf = df.groupby(['ContractId', pd.Grouper(freq='1D', level=1)]).sum()
+
+
+def daily_default_full_timeseries():
+    default = daily_sdf_fullts['PAR30+']
+    default_event = (default.diff() != 0) & (daily_sdf_fullts['PAR30+']==True)
+    recovery_event = (default.diff() != 0) & (daily_sdf_fullts['PAR30+']==False) & (
+        (default.index.get_level_values(1).month != 12) & (default.index.get_level_values(1).year != 2017)
+        )
+    daily_sdf_fullts[recovery_event].to_csv('recoveries.csv')
+    daily_sdf_fullts[default_event].to_csv('defauls.csv')
 
 
 
 #############  FREQUENTIST
 
 
-## 1) MONTH OF DEFAULT DOES NOT MATTER
+
 monthly_sdf = daily_sdf.groupby(['ContractId',pd.Grouper(freq='M', level=1)])['AmountPaid'].sum()
 monthly_sdf_pivot = monthly_sdf.unstack(0).fillna(0)
+
+cumulative_percent_sdf = create_percent_sdf(monthly_sdf_pivot, cumulative=True, cohort='dec_17')
+
 
 first_payment = monthly_sdf_pivot.iloc[1]
 
 first_payment[first_payment==0]
 
 
-####### INCLUDES WHERE THERE IS ZERO PAYMENT DUE TO COMPLETED CONTRACTS
-defaults = monthly_sdf_pivot.iloc[1:]==0
-individual_uncond_PD = defaults.sum()/defaults.count()
-unconditional_PD = defaults.sum().sum()/defaults.count().sum()
+
+## 1) MONTH OF DEFAULT DOES NOT MATTER
+
+
+defaults = monthly_sdf_pivot.iloc[1:]==0 # total non-NaN: 28,593
+paid = cumulative_percent_sdf >= 0.99
+
+## completed contracts are converted to NaN
+defaults = defaults.mask(paid).astype('boolean')
+
+individual_uncond_PD = defaults.sum(axis=0, skipna=True)/defaults.count(axis=0)  #df.count() does not count NaNs
+unconditional_PD = defaults.sum(axis=0, skipna=True).sum()/defaults.count(axis=0).sum()
 
 last_month_defaults = defaults.shift(1)
 
-D_given_D = defaults & last_month_defaults
-D_given_ND = defaults & ~last_month_defaults.astype('bool')
+D_given_D = defaults & last_month_defaults  # 2,233 events
+D_given_ND = defaults & ~last_month_defaults # 1,139
 
-ND_given_D = ~defaults.astype('bool') & last_month_defaults
-ND_given_ND = ~defaults.astype('bool') & ~last_month_defaults.astype('bool')
+ND_given_D = ~defaults & last_month_defaults # 1,060
+ND_given_ND = ~defaults & ~last_month_defaults #23,161
 
 
-PD_given_D = D_given_D.sum().sum() / D_given_D.count().sum()
+PD_given_D = D_given_D.sum().sum() / D_given_D.count().sum()  #2,233/29,024
 PD_given_ND = D_given_ND.sum().sum() / D_given_ND.count().sum()
 
-PND_given_D = ND_given_D.sum().sum() / ND_given_D.count().sum()
+PND_given_D = ND_given_D.sum().sum() / ND_given_D.count().sum() #1,060, 28,198
 PND_given_ND = ND_given_ND.sum().sum() / ND_given_ND.count().sum()
 
-PD_given_D + PD_given_ND + PND_given_D + PND_given_ND + 1/35. #(january 2017 not included)
+## for completeness
+paid_off_months = paid.sum().sum() # 6,407
+P_paid_off = paid_off_months / monthly_sdf_pivot.count().sum()  ## 6,407/36,000
+
+
+## why doesnt this equal 1
+PD_given_D + PD_given_ND + PND_given_D + PND_given_ND + P_paid_off
+
+np.sum([D_given_D.sum().sum(),
+    D_given_ND.sum().sum(),
+    ND_given_D.sum().sum(),
+    ND_given_ND.sum().sum(),
+    paid_off_months],)  #34,000 ==> 34 months ==> 2 months not included
 
 
 
@@ -63,5 +100,6 @@ PD_given_ND = D_given_ND.sum(axis=1) / D_given_ND.count(axis=1)
 PND_given_D = ND_given_D.sum(axis=1)/ ND_given_D.count(axis=1)
 PND_given_ND = ND_given_ND.sum(axis=1)/ ND_given_ND.count(axis=1)
 
+## also dont always sum to 1
 PD_given_D + PD_given_ND + PND_given_D + PND_given_ND
 
