b1fa95b1d19103f5770c7a7d8ddfaa4e43ecfdcf
Mark Wells <wellowdata@gmail.com>
2021-01-03 18:09:12 +0000

    Update AFC_PD_and_expected_repayment_model.py

--- AFC_PD_and_expected_repayment_model.py
+++ AFC_PD_and_expected_repayment_model.py
@@ -17,7 +17,37 @@ df = df.groupby(['ContractId', 'TransactionTS']).sum()
 
 daily_sdf = df.groupby(['ContractId', pd.Grouper(freq='1D', level=1)]).sum()
 monthly_sdf = daily_sdf.groupby(['ContractId',pd.Grouper(freq='M', level=1)])['AmountPaid'].sum()
-monthly_sdf_pivot = monthly_sdf.unstack(0).fillna(0)
+monthly_sdf_pivot = monthly_sdf['AmountPaid'].unstack(0).fillna(0)
 
 
-PD_given_D, PD_given_ND, PND_given_D, PND_given_ND = calc_PD(monthly_sdf_pivot, )
+PD_given_D, PD_given_ND, PND_given_D, PND_given_ND = calc_PD(monthly_sdf_pivot,)
+monthly_sdf_fullts = calc_moving_average(monthly_sdf)
+
+ma_pivot = monthly_sdf_fullts['MovingAverage'].unstack(0).shift(1).fillna(method='ffill') #shift(1) for next month forecast, ffill for future months with no MA (because no payments made)
+
+## logic
+
+"""
+
+if month = paid:
+    month+1 = paid*PNDgivenND + PDgivenND*0
+elif month != paid
+    month+1 = paid*PNDgivenD + PDgivenD*0
+    
+"""
+
+## SHOULD  PND_given_D + PND_given_ND = 1?
+
+one_month_forecast_if_paid = ma_pivot.multiply(PND_given_ND)
+one_month_forecast_if_default = ma_pivot.multiply(PND_given_D)
+
+
+one_contract_id = monthly_sdf_pivot.columns[3]
+
+one_ma = ma_pivot[one_contract_id]
+forecast_date = '2019-12-31'
+
+defaults = monthly_sdf_pivot.iloc[1:]==0 # total non-NaN: 28,593
+
+if defaults.loc[forecast_date, one_contract_id]:
+    
