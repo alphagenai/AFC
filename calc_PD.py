01890696227fed1d0586358f5dabb792cebe1140
Mark Wells <wellowdata@gmail.com>
2021-01-03 16:01:23 +0000

    created functions

--- calc_PD.py
+++ calc_PD.py
@@ -11,13 +11,16 @@ import numpy as np
 
 from individual_analysis1 import create_small_df, create_percent_sdf, convert_to_daily_pivot
 
-
-df = pd.read_pickle('files\\small_df_1000_dec_17.pkl')
-df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S').dt.tz_localize(None)
-
-df = df.groupby(['ContractId', 'TransactionTS']).sum()
-
-daily_sdf = df.groupby(['ContractId', pd.Grouper(freq='1D', level=1)]).sum()
+def main():
+    df = pd.read_pickle('files\\small_df_1000_dec_17.pkl')
+    df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S').dt.tz_localize(None)
+    
+    df = df.groupby(['ContractId', 'TransactionTS']).sum()
+    
+    daily_sdf = df.groupby(['ContractId', pd.Grouper(freq='1D', level=1)]).sum()
+    monthly_sdf = daily_sdf.groupby(['ContractId',pd.Grouper(freq='M', level=1)])['AmountPaid'].sum()
+    monthly_sdf_pivot = monthly_sdf.unstack(0).fillna(0)
+    calc_PD(monthly_sdf_pivot)
 
 
 def daily_default_full_timeseries():
@@ -34,74 +37,72 @@ def daily_default_full_timeseries():
 #############  FREQUENTIST
 
 
-
-monthly_sdf = daily_sdf.groupby(['ContractId',pd.Grouper(freq='M', level=1)])['AmountPaid'].sum()
-monthly_sdf_pivot = monthly_sdf.unstack(0).fillna(0)
-
-monthly_cumulative_percent_sdf_pivot = create_percent_sdf(monthly_sdf_pivot, cumulative=True, cohort='dec_17')
-
-
-first_payment = monthly_sdf_pivot.iloc[1]
-
-first_payment[first_payment==0]
-
-
-
-## 1) MONTH OF DEFAULT DOES NOT MATTER
-
-
-defaults = monthly_sdf_pivot.iloc[1:]==0 # total non-NaN: 28,593
-fully_paid = monthly_cumulative_percent_sdf_pivot >= 0.99
-
-## completed contracts are converted to NaN
-defaults = defaults.mask(fully_paid).astype('boolean')
-
-individual_uncond_PD = defaults.sum(axis=0, skipna=True)/defaults.count(axis=0)  #df.count() does not count NaNs
-unconditional_PD = defaults.sum(axis=0, skipna=True).sum()/defaults.count(axis=0).sum()
-
-last_month_defaults = defaults.shift(1)
-
-D_given_D = defaults & last_month_defaults  # 2,233 events
-D_given_ND = defaults & ~last_month_defaults # 1,139
-
-ND_given_D = ~defaults & last_month_defaults # 1,060
-ND_given_ND = ~defaults & ~last_month_defaults #23,161
-
-
-PD_given_D = D_given_D.sum().sum() / D_given_D.count().sum()  #2,233/29,024
-PD_given_ND = D_given_ND.sum().sum() / D_given_ND.count().sum()
-
-PND_given_D = ND_given_D.sum().sum() / ND_given_D.count().sum() #1,060, 28,198
-PND_given_ND = ND_given_ND.sum().sum() / ND_given_ND.count().sum()
-
-## for completeness
-paid_off_months = fully_paid.sum().sum() # 6,407
-P_paid_off = paid_off_months / monthly_sdf_pivot.count().sum()  ## 6,407/36,000
-
-
-## why doesnt this equal 1
-PD_given_D + PD_given_ND + PND_given_D + PND_given_ND + P_paid_off
-
-np.sum([D_given_D.sum().sum(),
-    D_given_ND.sum().sum(),
-    ND_given_D.sum().sum(),
-    ND_given_ND.sum().sum(),
-    paid_off_months],)  #34,000 ==> 34 months ==> 2 months not included
-
-
-
-
-## 2) MONTH MATTERS
-
-
-PD_given_D = D_given_D.sum(axis=1) / D_given_D.count(axis=1)
-PD_given_ND = D_given_ND.sum(axis=1) / D_given_ND.count(axis=1)
-
-PND_given_D = ND_given_D.sum(axis=1)/ ND_given_D.count(axis=1)
-PND_given_ND = ND_given_ND.sum(axis=1)/ ND_given_ND.count(axis=1)
-
-## also dont always sum to 1
-PD_given_D + PD_given_ND + PND_given_D + PND_given_ND
+def calc_PD(monthly_sdf_pivot, ):
+    
+    monthly_cumulative_percent_sdf_pivot = create_percent_sdf(monthly_sdf_pivot, cumulative=True, cohort='dec_17')
+    
+    
+    first_payment = monthly_sdf_pivot.iloc[1]
+    
+    first_payment[first_payment==0]
+    
+    
+    
+    ## 1) MONTH OF DEFAULT DOES NOT MATTER
+    
+    
+    defaults = monthly_sdf_pivot.iloc[1:]==0 # total non-NaN: 28,593
+    fully_paid = monthly_cumulative_percent_sdf_pivot >= 0.99
+    
+    ## completed contracts are converted to NaN
+    defaults = defaults.mask(fully_paid).astype('boolean')
+    
+    individual_uncond_PD = defaults.sum(axis=0, skipna=True)/defaults.count(axis=0)  #df.count() does not count NaNs
+    unconditional_PD = defaults.sum(axis=0, skipna=True).sum()/defaults.count(axis=0).sum()
+    
+    last_month_defaults = defaults.shift(1)
+    
+    D_given_D = defaults & last_month_defaults  # 2,233 events
+    D_given_ND = defaults & ~last_month_defaults # 1,139
+    
+    ND_given_D = ~defaults & last_month_defaults # 1,060
+    ND_given_ND = ~defaults & ~last_month_defaults #23,161
+    
+    
+    PD_given_D = D_given_D.sum().sum() / D_given_D.count().sum()  #2,233/29,024
+    PD_given_ND = D_given_ND.sum().sum() / D_given_ND.count().sum()
+    
+    PND_given_D = ND_given_D.sum().sum() / ND_given_D.count().sum() #1,060, 28,198
+    PND_given_ND = ND_given_ND.sum().sum() / ND_given_ND.count().sum()
+    
+    ## for completeness
+    paid_off_months = fully_paid.sum().sum() # 6,407
+    P_paid_off = paid_off_months / monthly_sdf_pivot.count().sum()  ## 6,407/36,000
+    
+    
+    ## why doesnt this equal 1
+    PD_given_D + PD_given_ND + PND_given_D + PND_given_ND + P_paid_off
+    
+    np.sum([D_given_D.sum().sum(),
+        D_given_ND.sum().sum(),
+        ND_given_D.sum().sum(),
+        ND_given_ND.sum().sum(),
+        paid_off_months],)  #34,000 ==> 34 months ==> 2 months not included
+    
+    return PD_given_D, PD_given_ND, PND_given_D, PND_given_ND
+    
+    
+    ## 2) MONTH MATTERS
+    
+    
+    PD_given_D = D_given_D.sum(axis=1) / D_given_D.count(axis=1)
+    PD_given_ND = D_given_ND.sum(axis=1) / D_given_ND.count(axis=1)
+    
+    PND_given_D = ND_given_D.sum(axis=1)/ ND_given_D.count(axis=1)
+    PND_given_ND = ND_given_ND.sum(axis=1)/ ND_given_ND.count(axis=1)
+    
+    ## also dont always sum to 1
+    PD_given_D + PD_given_ND + PND_given_D + PND_given_ND
 
 
 
@@ -109,3 +110,7 @@ PD_given_D + PD_given_ND + PND_given_D + PND_given_ND
 #### BAYESIAN
 
 ## TO DO
+
+
+if __name__ == "__main__":
+    main()
--- monthly_averages.py
+++ monthly_averages.py
@@ -13,29 +13,30 @@ import numpy as np
 
 from individual_analysis1 import create_small_df, create_percent_sdf, convert_to_daily_pivot
 
+def main():
+    df = pd.read_pickle('files\\small_df_1000_dec_17.pkl')
+    df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S').dt.tz_localize(None)
+    
+    df = df.groupby(['ContractId', 'TransactionTS']).sum()
+    
+    monthly_sdf = df.groupby(['ContractId', pd.Grouper(freq='M', level=1)]).sum()
+    calc_moving_average(monthly_sdf)
 
-df = pd.read_pickle('files\\small_df_1000_dec_17.pkl')
-df['TransactionTS'] = pd.to_datetime(df['TransactionTS'],format='%Y/%m/%d %H:%M:%S').dt.tz_localize(None)
+def calc_moving_average(monthly_sdf):
 
-df = df.groupby(['ContractId', 'TransactionTS']).sum()
+    date_idx = pd.date_range('31/12/2017', '30-11-2020', freq='M')
+    idx = pd.MultiIndex.from_product(
+        [monthly_sdf.index.levels[0], date_idx], names=['ContractId', 'TransactionTS'])
+    
+    monthly_sdf_fullts = monthly_sdf.reindex(idx,).fillna(0)    
+    
+    monthly_sdf_fullts['paid'] = monthly_sdf_fullts['AmountPaid']!=0
+    
+    ## need to drop the first month and its not currently grouping by contractID properly
+     
+    monthly_sdf_fullts["MovingAverage"] = monthly_sdf_fullts.groupby(['ContractId', 'paid',])['AmountPaid'].rolling(6).mean().unstack(1)[True].droplevel(level=0)
+    return monthly_sdf_fullts
 
-daily_sdf = df.groupby(['ContractId', pd.Grouper(freq='1D', level=1)]).sum()
-daily_sdf_pivot = daily_sdf['AmountPaid'].unstack(0).fillna(0)
-daily_percent_pivot = create_percent_sdf(daily_sdf_pivot, cumulative=False, use_monthdiff=False, cohort='dec_17')
 
-
-monthly_sdf = daily_sdf.groupby(['ContractId',pd.Grouper(freq='M', level=1)])['AmountPaid'].sum().to_frame()
-
-date_idx = pd.date_range('31/12/2017', '30-11-2020', freq='M')
-idx = pd.MultiIndex.from_product(
-    [monthly_sdf.index.levels[0], date_idx], names=['ContractId', 'TransactionTS'])
-
-monthly_sdf_fullts = monthly_sdf.reindex(idx,).fillna(0)
-
-monthly_percent_pivot = daily_percent_pivot.groupby(pd.Grouper(freq='M')).sum()
-
-monthly_sdf_fullts['paid'] = monthly_sdf_fullts['AmountPaid']!=0
-
-## need to drop the first month and its not currently grouping by contractID properly
- 
-monthly_sdf_fullts["MovingAverage"] = monthly_sdf_fullts.groupby(['ContractId', 'paid',])['AmountPaid'].rolling(6).mean().unstack(1)[True].droplevel(level=0)
+if __name__ == "__main__":
+    main()
