fa5a65c5e0f2fad14c61833d1360aa1885d719bd
Mark Wells <wellowdata@gmail.com>
2021-01-04 19:14:07 +0000

    added KDE plots through time

--- AFC_PD_and_expected_repayment_model.py
+++ AFC_PD_and_expected_repayment_model.py
@@ -7,8 +7,10 @@ Created on Thu Dec 10 13:27:40 2020
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 from scipy.stats import binom
+from matplotlib import pyplot as plt
 
 from calc_PD import calc_PD
 from monthly_averages import calc_moving_average
@@ -50,6 +52,8 @@ class Node(object):
         self.t = t
         self.value = val
         if prev_node is not None:
+            self.cum_val = prev_node.cum_val+val
+
             prev_paid = (prev_node.value != 0)
             paid = (val != 0)
             if prev_paid & paid:
@@ -62,6 +66,23 @@ class Node(object):
                 self.p = prev_node.p*PD_given_D
             else:
                 raise ValueError('Something went wrong')
+        else:
+            self.cum_val = val
+
+
+
+def weighted_kde(x, weights, **kwargs):
+    sns.kdeplot(x, weights=weights, **kwargs)
+
+# Define and use a simple function to label the plot in axes coordinates
+def label(x, color, label):
+    ax = plt.gca()
+    ax.text(0, .2, label, fontweight="bold", color=color,
+            ha="left", va="center", transform=ax.transAxes)
+
+
+
+
 
 class LatticeModel(object):
     def __init__(self, initial_payment, average_payment, 
@@ -89,7 +110,67 @@ class LatticeModel(object):
             node.offspring2 = Node(t+1, 0, node)
             new_nodes.extend([node.offspring1, node.offspring2])
         self.nodes_dict[t+1] = new_nodes
+
+    def plot_forecasts(self, T):
+        t_list = [node.t for tau in range(T) for node in lm.nodes_dict[tau]]
+        p_list = [node.p for tau in range(T) for node in lm.nodes_dict[tau]]
+        v_list = [node.cum_val for tau in range(T) for node in lm.nodes_dict[tau]]
+        
+        df = pd.DataFrame(dict(t=t_list, v=v_list, p=p_list))
+
+        sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+        
+        # Initialize the FacetGrid object
+        pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
+        g = sns.FacetGrid(df, row="t", hue="t", aspect=15, height=.5, palette=pal)
+        
+            
+        # Draw the densities in a few steps
+        g.map(weighted_kde, "v", "p",
+              bw_adjust=.5, 
+              clip_on=False,
+              fill=True, 
+              alpha=1, linewidth=1.5)
+        g.map(weighted_kde, "v", "p", 
+              clip_on=False, color="w", lw=2, bw_adjust=.5)
+        g.map(plt.axhline, y=0, lw=2, clip_on=False)
+        
+
+        g.map(label, "v")
+        
+        # Set the subplots to overlap
+        g.fig.subplots_adjust(hspace=-.25)
+        
+        # Remove axes details that don't play well with overlap
+        g.set_titles("")
+        g.set(yticks=[])
+        g.despine(bottom=True, left=True)        
         
         
-lm = LatticeModel(initial_payment, average_payment, contract_id=one_contract_id)
-lm.add_level()
+if __name__ == "__main__":
+    lm = LatticeModel(initial_payment, average_payment, contract_id=one_contract_id)
+    lm.add_level()
+    lm.add_level()
+    
+    x = [node.cum_val for node in lm.nodes_dict[2]]
+    height = [node.p for node in lm.nodes_dict[2]]
+    sns.barplot(x=x, y=height)
+    
+    lm.add_level()
+    x = [node.cum_val for node in lm.nodes_dict[3]]
+    height = [node.p for node in lm.nodes_dict[3]]
+    sns.barplot(x=x, y=height)
+    
+    
+    lm.add_level()
+    x = [node.cum_val for node in lm.nodes_dict[4]]
+    height = [node.p for node in lm.nodes_dict[4]]
+    sns.barplot(x=x, y=height)
+    
+    
+    lm.add_level()
+    x = [node.cum_val for node in lm.nodes_dict[5]]
+    height = [node.p for node in lm.nodes_dict[5]]
+    sns.barplot(x=x, y=height)
+    
+
