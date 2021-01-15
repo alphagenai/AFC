badabe029cdcb6d352bb00776cfb8febc6af8b8f
Mark Wells <wellowdata@gmail.com>
2021-01-15 13:05:40 +0000

    Update PAR30_analysis.py

--- PAR30_analysis.py
+++ PAR30_analysis.py
@@ -5,40 +5,114 @@ Created on Tue Jan 12 11:54:59 2021
 @author: mark
 """
 
-########## TRANSITION PROBABILITIES FROM PAR30 STATE
 
-par30_daily_pivot = daily_sdf_fullts['PAR30+'].unstack(0).sort_index()
+from calc_PD import how_many_true
+
+for cutoff in [0.8, 0.99]:
+    par30_daily_pivot = daily_sdf_fullts['PAR30+'].unstack(0).sort_index()
+
+    ## completed contracts are converted to NaN
+    ## be careful that the start and end dates of both dataframes is the same
+    fully_paid = daily_cumulative_percent_sdf_pivot.shift(1) >= cutoff #final payment is not included in fully paid flag
+    par30_daily_pivot = par30_daily_pivot.mask(fully_paid).astype('boolean')
+    
+    daily_cumulative_percent_sdf_pivot.loc[:,par30_daily_pivot.any(axis=0)].plot()
+    daily_cumulative_percent_sdf_pivot.loc[:,par30_daily_pivot.any(axis=0)].to_csv('PAR30_examples.csv')
+    par30_daily_pivot.loc[:, par30_daily_pivot.any(axis=0)].to_csv('Par_30_examples2.csv')
+    
+    ## seems right - this guy has a break in paying for July/early august
+    #daily_cumulative_percent_sdf_pivot.loc['June-2018':'Aug-2018','1349716'].plot()
+    
+    unconditional_daily_PE = par30_daily_pivot.sum().sum() / par30_daily_pivot.count().sum() 
+    
+    
+    transition_df = pd.concat([transition_to_par30.sum(), transition_from_par30.sum()], axis=1) #.to_csv('transition_probs.csv')
+    
+    counterparty_events = transition_df.astype('bool').sum()  #recovery happens at least once to 413 counterparties
+    number_of_events = transition_df.sum()
+    print('cutoff {}: number of events: {}'.format(cutoff, number_of_events))
+    
+    recovery = (transition_df[0] == transition_df[1]) & transition_df[0].astype('bool')  
+    recovery.sum() # recovery happens EVERY TIME to 319 counterparties
+    
+    probability_of_recovery = transition_df.sum()[1] / transition_df.sum()[0]  # 1,359/1,466 
+    print('cutoff {}, probability_of_recovery: {}'.format(cutoff, probability_of_recovery))
+
+    unconditional_probability_of_PAR30_given_paying = transition_df.sum()[0]/total_days_of_nonPar30
 
 
-## completed contracts are converted to NaN
-## be careful that the start and end dates of both dataframes is the same
-fully_paid = daily_cumulative_percent_sdf_pivot.shift(1) >= 0.80 #final payment is not included in fully paid flag
-par30_daily_pivot = par30_daily_pivot.mask(fully_paid).astype('boolean')
+def transition_probs(par30_daily_pivot):
+    par30_yesterday_pivot = par30_daily_pivot.shift(1)
+    
+    transition_to_par30 = ~par30_yesterday_pivot & par30_daily_pivot 
+    transition_from_par30 = par30_yesterday_pivot & ~par30_daily_pivot 
+    stay_in_non_par30 = ~par30_yesterday_pivot & ~par30_daily_pivot  # 816,190
+    stay_in_par30 = par30_yesterday_pivot & par30_daily_pivot  # 82,740
+    
+    total_days_of_nonPar30 = (~par30_daily_pivot).sum().sum() #818,609
 
-daily_cumulative_percent_sdf_pivot.loc[:,par30_daily_pivot.any(axis=0)].plot()
-daily_cumulative_percent_sdf_pivot.loc[:,par30_daily_pivot.any(axis=0)].to_csv('PAR30_examples.csv')
-par30_daily_pivot.loc[:, par30_daily_pivot.any(axis=0)].to_csv('Par_30_examples2.csv')
+    """ TO DO: FIX THIS: """
+    ### REMEMBER FALSE & NA = FALSE!!
+    P3_given_P3 = par30_daily_pivot & par30_yesterday_pivot  
+    P3_given_NP3 = par30_daily_pivot & ~par30_yesterday_pivot  
+    
+    logging.info('P3_given_P3: {}'.format(how_many_true(P3_given_P3)))
+    logging.info('P3_given_NP3: {}'.format(how_many_true(P3_given_NP3)))
 
-## seems right
-#daily_cumulative_percent_sdf_pivot.loc['June-2018':'Aug-2018','1349716'].plot()
+    NP3_given_P3 = ~par30_daily_pivot & par30_yesterday_pivot  
+    NP3_given_NP3 = ~par30_daily_pivot & ~par30_yesterday_pivot  
 
-unconditional_daily_PE = par30_daily_pivot.sum().sum() / par30_daily_pivot.count().sum() 
+    logging.info('NP3_given_P3: {}'.format(how_many_true(NP3_given_P3)))
+    logging.info('NP3_given_NP3: {}'.format(how_many_true(NP3_given_NP3)))
 
-par30_yesterday_pivot = par30_daily_pivot.shift(1)
+    NA_given_P3 = par30_daily_pivot.isna() & par30_yesterday_pivot # 
+    NA_given_NP3 = par30_daily_pivot.isna() & ~par30_yesterday_pivot # 
 
-transition_to_par30 = ~par30_yesterday_pivot & par30_daily_pivot 
-transition_from_par30 = par30_yesterday_pivot & ~par30_daily_pivot 
+    logging.info('NA_given_P3: {}'.format(how_many_true(NA_given_P3)))
+    logging.info('NA_given_NP3: {}'.format(how_many_true(NA_given_NP3)))
+    return point_estimate(P3_given_P3, P3_given_NP3, NP3_given_P3, NP3_given_NP3)
 
-transition_df = pd.concat([transition_to_par30.sum(), transition_from_par30.sum()], axis=1) #.to_csv('transition_probs.csv')
+def point_estimate(D_given_D, D_given_ND, ND_given_D, ND_given_ND): 
+    """ from calc_PD """
+    total_given_D = how_many_true(D_given_D) + how_many_true(ND_given_D)
+    total_given_ND = how_many_true(D_given_ND) + how_many_true(ND_given_ND)
 
-counterparty_events = transition_df.astype('bool').sum()  #recovery happens at least once to 413 counterparties
-number_of_events = transition_df.sum()
+    PD_given_D = how_many_true(D_given_D) / total_given_D 
+    PD_given_ND = how_many_true(D_given_ND) / total_given_ND # = (1 - PD_given_D)
+    PND_given_D = how_many_true(ND_given_D) / total_given_D # = (1 - PND_given_ND)
+    PND_given_ND = how_many_true(ND_given_ND) / total_given_ND 
 
-recovery = (transition_df[0] == transition_df[1]) & transition_df[0].astype('bool')  
-recovery.sum() # recovery happens EVERY TIME to 319 counterparties
+    logging.info('PD_given_D : {} / {}'.format(how_many_true(D_given_D) , total_given_D))    
+    logging.info('PD_given_ND : {} / {}'.format(how_many_true(D_given_ND) , total_given_ND))    
+    logging.info('PND_given_D : {} / {}'.format(how_many_true(ND_given_D) , total_given_D))    
+    logging.info('PND_given_ND : {} / {}'.format(how_many_true(ND_given_ND) , total_given_ND))    
 
-probability_of_recovery = transition_df.sum()[1] / transition_df.sum()[0]  # 1,359/1,466 
+   
+    PD_dict = {'PD_given_D':PD_given_D, 
+               'PD_given_ND':PD_given_ND, 
+               'PND_given_D':PND_given_D, 
+               'PND_given_ND':PND_given_ND,
+               }
+    PD_dict = PD_dict ## TO DO: Diferentiate between the different types of PD
+    return PD_dict
 
+def calc_monthly_transition_probs(PD_dict):
+    monthly_transition_dict = {}
+    for k, v in PD_dict.items():
+        print(k)
+        if k in ['PD_given_ND', 'PND_given_D']:
+            monthly_prob = transition_prob_to_monthly(v)
+            print(monthly_prob*100)
+            monthly_transition_dict[k] = monthly_prob()
+    monthly_transition_dict['PD_given_D'] = 1 - 'PD_given_ND'
+    monthly_transition_dict['PND_given_ND'] = 1 - 'PND_given_D'
+    monthly_transition_dict.to_pickle('monthly_transition_probs.pkl')
+    return monthly_transition_dict
+
+
+def transition_prob_to_monthly(prob):
+    """ At least one transition in a month == 1 - probability(no transition)"""
+    return 1 - ((1-prob)**30)
 
 """ 
 TO DO: investigate sequences of par30 and how long they tend to be. 
