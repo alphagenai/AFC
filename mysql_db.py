7de91e449abd29e77c71df9215685489343eb4da
Mark Wells <wellowdata@gmail.com>
2021-02-03 11:45:09 +0000

    Create mysql_db.py

---
+++ mysql_db.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 15:47:40 2021
+
+@author: mark
+"""
+
+import google_sa_auth
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='nj+xRF91',
+                              host='localhost',
+                              database='afc')
