b8fb85a8314fdae73644b412302eb4de90336388
Mark Wells <wellowdata@gmail.com>
2021-02-03 11:45:32 +0000

    Update mysql_db.py

--- mysql_db.py
+++ mysql_db.py
@@ -5,7 +5,7 @@ Created on Tue Feb  2 15:47:40 2021
 @author: mark
 """
 
-import google_sa_auth
+#import google_sa_auth
 import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password='nj+xRF91',
