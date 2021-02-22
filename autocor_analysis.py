c045ff8aaae7cd2f175e3e27564604ae8d1ae18d
Mark Wells <wellowdata@gmail.com>
2021-02-22 13:53:32 +0000

    Update autocor_analysis.py

--- autocor_analysis.py
+++ autocor_analysis.py
@@ -14,17 +14,31 @@ from statsmodels.graphics.tsaplots import plot_acf
 import statsmodels.api as sm
 from matplotlib import pyplot as plt
 
-acf_dict = {}
+from basic_datasets import MONTHLY_UNFINISHED_CONTRACTS, MONTHLY_SDF_PIVOT
 
-for col in monthly_percent_pivot.columns:
-    s = monthly_percent_pivot.loc[:, col]
+
+unfinished_contracts = MONTHLY_UNFINISHED_CONTRACTS
+monthly_sdf_pivot = MONTHLY_SDF_PIVOT
+
+acf_dict1 = {}
+acf_dict2 = {}
+
+for cprty in unfinished_contracts.columns:
+    s1 = unfinished_contracts.loc[:, cprty]
+    s2 = monthly_sdf_pivot.loc[:, cprty]
+
     #plot_acf(s)
-    acf_dict[col] = pd.Series(sm.tsa.acf(s, nlags=12))
+
+    acf_dict1[cprty] = pd.Series(sm.tsa.acf(s1, nlags=12))
+    acf_dict2[cprty] = pd.Series(sm.tsa.acf(s2, nlags=12))
     
-df = pd.DataFrame.from_dict(acf_dict)
+df1 = pd.DataFrame.from_dict(acf_dict1)
+df2 = pd.DataFrame.from_dict(acf_dict2)
 
-df.mean(axis=1)
+#print(df1.mean(axis=1))
+#print(df2.mean(axis=1))
 
+best_params = df1.mean(axis=1).loc[1:6]
 
 """
 TO LOOK INTO 
