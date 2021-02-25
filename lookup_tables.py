492f4823d8f62798095a88954eaa9ccca9c3ecc5
Mark Wells <wellowdata@gmail.com>
2021-02-25 13:30:36 +0000

    Update lookup_tables.py

--- lookup_tables.py
+++ lookup_tables.py
@@ -13,15 +13,16 @@ import numpy as np
 
 from basic_datasets import BasicDatasets
 
+bd = BasicDatasets()
 
-""" NEED TO REMOVE FULLY PAID TIME PERIODS """
-
-
-bds = BasicDatasets()
-
-dfts = bds.daily_full_ts
 dcpp = bds.daily_cumulative_percent_sdf_pivot
-dpp = bds.daily_percent_pivot.mask(bds.daily_full_paid)
+dpp = bds.daily_percent_pivot.mask(bds.daily_fully_paid)
+
+dfts = pd.merge(bds.daily_full_ts,
+    dcpp.stack().swaplevel().to_frame('cum_val'),
+    left_index=True,
+    right_index=True)
+
 
 ultimate_loss = (1 - dcpp.iloc[-1]).clip(0,1).to_frame('Ulitimate_loss')
 
@@ -32,7 +33,7 @@ lookback_average_pay = {}
 idx = pd.IndexSlice
 
 #data_df = dfts['days_out_of_elec'].loc[idx[:,'Jan-2018':'Oct-2020'],].unstack(1)
-df = dfts['days_out_of_elec'].loc[idx[:,'Jan-2018':'Oct-2020'],]
+df = dfts[dfts['cum_val']<0.99].loc[idx[:,'Jan-2018':'Oct-2020'],]['days_out_of_elec']
 
 
 #df = data_df.join(ultimate_loss, )
@@ -50,18 +51,37 @@ bins = [(x-1)/10000. for x in range(21)] + [(x+2)/1000. for x in range(8)] + [(x
 
 d_dict = {}
 a_dict = {}
-for i in range(30, np.min([days_in_ts, 360]), 10):
-    dg = (df.clip(upper=i) == i).to_frame('lookback_{}'.format(i)).join(ultimate_loss).groupby(['lookback_{}'.format(i), 'TransactionTS'])    
+
+d_count_dict = {}
+a_count_dict = {}
+
+for i in range(10, np.min([days_in_ts, 190]), 10):
+    dg = (df.clip(upper=i) == i).to_frame('lookback_{}'.format(i)).join(ultimate_loss).groupby(['lookback_{}'.format(i), 'TransactionTS'])
     rolling_mean_df = pd.cut(dpp.rolling(i).mean().stack().clip(0,1), bins=bins, ).to_frame('lookback_{}'.format(i)).join(ultimate_loss)
     ag = rolling_mean_df.groupby(['TransactionTS', 'lookback_{}'.format(i)])
     # optional
-    ag.dropna(axis=1, how='all', inplace=True)
-    
     # dg.count().unstack(0) # for debugging
     # ag.count().unstack(1) 
 
     d_dict[i] = dg.mean().unstack(0)
-    a_dict[i] = ag.mean().unstack(1) 
+    a_dict[i] = ag.mean().unstack(1).dropna(axis=1, how='all',)
 
-pd.concat(d_dict.values(), axis=1, keys=d_dict.keys()).to_csv('default_lookup_table.csv')
-pd.concat(a_dict.values(), axis=1, keys=a_dict.keys()).to_csv('average_payment_table.csv')
+    d_count_dict[i] = dg.count().unstack(0)
+    a_count_dict[i] = ag.count().unstack(1).dropna(axis=1, how='all',)
+
+
+pd.concat(d_dict.values(), axis=1, keys=d_dict.keys()).to_csv('files\\default_lookup_table.csv')
+pd.concat(a_dict.values(), axis=1, keys=a_dict.keys()).to_csv('files\\average_payment_table.csv')
+
+pd.concat(d_count_dict.values(), axis=1, keys=d_count_dict.keys()).to_csv('files\\default_counts.csv')
+pd.concat(a_count_dict.values(), axis=1, keys=a_dict.keys()).to_csv('files\\average_payment_counts.csv')
+
+
+""" 
+    EAD - amount left to pay at default
+    
+    amount of time into the contract that default occurs
+
+    look into the wierd transactions that happen on the last day of the month
+
+"""
