9b39e1904270a7f5df23324f01a86a4e18355c14
Mark Wells <wellowdata@gmail.com>
2021-03-02 20:15:25 +0000

    Update Counterparty.py

--- Counterparty.py
+++ Counterparty.py
@@ -21,15 +21,16 @@ class Counterparty(object):
         self.PD = 0.055
         self.RSQ = 0.3
         
+        ## Beta priors
         self.PD_given_D_alpha = 1.1
         self.PD_given_D_beta = 6
         self.PD_given_ND_alpha = 1.1
         self.PD_given_ND_beta = 3
         
-        self.params = {'PD_given_D_alpha':1.1,
-                      'PD_given_D_beta':6,
-                      'PD_given_ND_alpha':1.1,
-                      'PD_given_ND_beta':3,
+        self.params = {'PD_given_D_alpha':self.PD_given_D_alpha,
+                      'PD_given_D_beta':self.PD_given_D_beta,
+                      'PD_given_ND_alpha':self.PD_given_ND_alpha,
+                      'PD_given_ND_beta':self.PD_given_ND_beta,
                       }
         
         
@@ -40,6 +41,11 @@ class Counterparty(object):
         """ updates alpha and beta using calc_PD """
     
         bpdu = BayesianPDUpdater(self.params)
+ 
+        self.PD_given_D_alpha = self.params['PD_given_D_alpha']
+        self.PD_given_D_beta = self.params['PD_given_D_beta']
+        self.PD_given_ND_alpha = self.params['PD_given_ND_alpha']
+        self.PD_given_ND_beta = self.params['PD_given_ND_beta']
         
         if pd_calc is None:
             pd_calc = PDCalculator(monthly_df_pivot)    
@@ -52,6 +58,11 @@ class Counterparty(object):
         hist_df = pd.concat([hist_d, hist_prev_d], axis=1)    
             
         self.params = bpdu.update_PD(hist_df)
+        self.PD_given_D_alpha = self.params['PD_given_D_alpha']
+        self.PD_given_D_beta = self.params['PD_given_D_beta']
+        self.PD_given_ND_alpha = self.params['PD_given_ND_alpha']
+        self.PD_given_ND_beta = self.params['PD_given_ND_beta']
+
         
     def update_bayesian_mean_PDs(self, monthly_df_pivot, forecast_startdate, pd_calc=None):
         self.update_PD_params(monthly_df_pivot, forecast_startdate, pd_calc=pd_calc)
@@ -70,6 +81,12 @@ class Counterparty(object):
             [(key, value) for key, value in self.PD_dict.items()]
             ))
 
+    @property
+    def PD_df(self):
+        pd_df = pd.DataFrame.from_dict(
+            self.PD_dict, orient='index', columns=['Probs'])
+        pd_df.index.rename('{}'.format(self.ContractId), inplace=True)
+        return pd_df
 
 
 
