476864cf91a3e987a63a7711cbd7f5cb4abff08e
Mark Wells <wellowdata@gmail.com>
2021-03-06 14:22:20 +0000

    one month electricity usage regression model

--- one_month_ahead_model.py
+++ one_month_ahead_model.py
@@ -5,8 +5,11 @@ Created on Fri Mar  5 21:34:24 2021
 @author: mark
 """
 
+import pandas as pd
+import statsmodels.api as sm
 
 from sklearn import linear_model
+from matplotlib import pyplot as plt
 
 from basic_datasets import BasicDatasets
 from sklearn.model_selection import TimeSeriesSplit
@@ -17,8 +20,8 @@ mfp = bd.monthly_fully_paid
 
 dfts = bd.daily_ts #full dataset excluding paid off contracts
 monthly_days_of_elec = (~dfts['elec_is_off']).groupby(['ContractId', pd.Grouper(freq='1M', level=1)]).sum()
-mde_pivot = monthly_days_of_elec.unstack(0)
-mde_perc_pivot = mde_pivot.divide(monthly_elec_pivot.index.daysinmonth, axis=0)
+mde_pivot = monthly_days_of_elec.unstack(0).mask(mfp)
+mde_perc_pivot = mde_pivot.divide(mde_pivot.index.daysinmonth, axis=0)
 
 
 reg = linear_model.LinearRegression()
@@ -31,11 +34,40 @@ reg.fit(X_train,y_train)
 reg.coef_
 y_pred = reg.predict(X_test)
 
-tscv = TimeSeriesSplit(6, max_train_size=6)
+# tscv = TimeSeriesSplit(6, max_train_size=6)
 
 
-for train_index, test_index in tscv.split(mde_perc_pivot):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+# for train_index, test_index in tscv.split(mde_perc_pivot):
+#     print("TRAIN:", train_index, "TEST:", test_index)
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
 
+ds_list = []
+X_train_list = []
+X_test_list = {}
+
+for i in range(len(mde_perc_pivot)-8+1): #+1 for start month 
+    ## start in Jan (index 1) end at 6months_x + 1 month_y + 1 bcos python 
+    ds = mde_perc_pivot.iloc[i+1:i+8].dropna(axis=1, how='any')
+    X_train = ds.iloc[0:6].T
+    y_train = ds.iloc[6].T
+    reg.fit(X_train,y_train)
+    #print(y_train.name, reg.coef_)
+    ds_list.append(ds.reset_index().drop(columns=['TransactionTS']))
+
+
+full_train_set = pd.concat(ds_list, axis=1).T
+
+X_train, y_train = full_train_set.loc[:, 0:5], full_train_set[6]
+reg.fit(X_train,y_train)
+print(y_train.name, reg.coef_)
+
+mod = sm.OLS(y_train, sm.add_constant(X_train))
+res = mod.fit()
+
+print(res.summary())
+
+fig, ax = plt.subplots()
+x = X_train.iloc[:, -1]
+ax.scatter(X_train.iloc[:, -2], y_train, color='green')
+ax.scatter(x, y_train, color='red')
